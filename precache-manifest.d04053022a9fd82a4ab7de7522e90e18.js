7591928b691ddb29e97a9f06ff0272c02b46a73c
Adrian Gonzalo <gonzahh@gmail.com>
2020-01-28 23:03:51 +0100

    Updates

---
+++ precache-manifest.d04053022a9fd82a4ab7de7522e90e18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763cf47bcd807d25084fe4dd68360795",
+    "url": "/speech-to-twitter-app/index.html"
+  },
+  {
+    "revision": "3d7581d1eeca2cec8aff",
+    "url": "/speech-to-twitter-app/static/css/main.0a27195e.chunk.css"
+  },
+  {
+    "revision": "e09f696b1bdee58e79b7",
+    "url": "/speech-to-twitter-app/static/js/2.0ab76594.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/speech-to-twitter-app/static/js/2.0ab76594.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d7581d1eeca2cec8aff",
+    "url": "/speech-to-twitter-app/static/js/main.2dabf1ff.chunk.js"
+  },
+  {
+    "revision": "f4c83bdaa78ec1348be6",
+    "url": "/speech-to-twitter-app/static/js/runtime-main.f7a8da24.js"
+  }
+]);
